178c993e068691f90991d92574186bf76b5b1f2a
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-05-16 09:11:38 +0530

    Use Encapsulation to Bind Data with methods

--- src/com/dembla/inheritance/practice/SingleInheritance.java
+++ src/com/dembla/inheritance/practice/SingleInheritance.java
@@ -3,19 +3,25 @@ package com.dembla.inheritance.practice;
 public class SingleInheritance extends One {
 
    public void print_two(){
+       value = 90 ;
        System.out.println("Two") ;
        print_one();
    }
 
     public static void main(String[] args) {
         new SingleInheritance().print_two();
+
+
     }
 
 }
 
 
 class One{
-    public void print_one(){
+
+    protected int value ;
+
+    protected void print_one(){
         System.out.println("One") ;
     }
 }
