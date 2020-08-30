e3304709d1ef96fc341e0a343f8dafa13c59b31f
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-08-31 01:41:20 +0530

    Matching Operations
    This is to match stream elements to some criteria and if it is matching then a Boolean value is returned.
    
    There we have 3 operations.
    	1. AnyMatch
    	2. NoneMatch
    AllMatch

--- src/main/java/com/dembla/jvm/lamdasfp/lecture5/StreamOperations.java
+++ src/main/java/com/dembla/jvm/lamdasfp/lecture5/StreamOperations.java
@@ -100,6 +100,29 @@ public class StreamOperations {
        // titleStream.forEach(System.out::println);
     }
 
+    // Queries:
+    //  (a) Is there at least one highly rated book (>= 4.8) that is inexpensive (<= $50)?
+    //  (b) Do all the books have a rating >= 4.8
+    //  (c) Check if none of books have bad rating (2.0)?
+    private static void match(List<Book> books) {
+        System.out.println("\nMatching ... ");
+        boolean anyMatch = books.stream()
+                .anyMatch(d -> d.getRating() >= 4.8 && d.getPrice() <= 50.0);
+        System.out.println("anyMatch? " + anyMatch);
+
+        boolean allMatch = books.stream()
+                .allMatch(d -> d.getRating() >= 4.8);
+        //.noneMatch(d -> d.getRating() < 4.8);
+        System.out.println("allMatch? " + allMatch);
+
+        boolean noneMatch = books.stream()
+                .noneMatch(d -> d.getRating() <= 2.0);
+        // .allMatch(d -> d.getRating() > 2.0);
+        //.anyMatch(d -> d.getRating() <= 2.0);
+        System.out.println("noneMatch? " + noneMatch);
+
+    }
+
     public static void main(String[] args) {
 
         List<Book> books = new ArrayList<>();
@@ -108,6 +131,9 @@ public class StreamOperations {
         books.addAll(DataExtractor.getFromBarnesAndNoble("java"));
 
         slice(books);
+
+        // terminal operations (return non-stream objects)
+        match(books);  // matching stream elements to some criteria
     }
 
 }
